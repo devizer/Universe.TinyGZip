d92805719b1e4ec90f9256e1be3461ded29fb566
devizer <vgoyda@gmail.com>
2016-08-03 10:05:54 +0300

    update

--- travis-plugins/sample-ffmpeg.sh
+++ travis-plugins/sample-ffmpeg.sh
@@ -12,7 +12,7 @@ $HOME/bin/ffmpeg -i $file -y \
   -preset ultrafast \
   -scodec copy -threads 3 -strict experimental \
   -c:a copy \
-  -x265-params crf=18:crf-min=15:crf-max=18:pools=4 \
+  -x265-params crf=21:crf-min=20:crf-max=22:pools=4 \
   - | pv > $file.mkv
   
 echo '************* convert done ****************'
