6836b9f428222056ab63edd037a810e58f978ad3
devizer <vgoyda@gmail.com>
2016-08-03 10:00:50 +0300

    update

--- travis-plugins/sample-ffmpeg.sh
+++ travis-plugins/sample-ffmpeg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 file=/tmp/big_buck_bunny_1080p_H264_AAC_25fps_7200K.mp4
-wget -O $file http://download.openbricks.org/sample/H264/big_buck_bunny_1080p_H264_AAC_25fps_7200K.MP4
+# wget -O $file http://download.openbricks.org/sample/H264/big_buck_bunny_1080p_H264_AAC_25fps_7200K.MP4
+wget -O $file http://download.openbricks.org/sample/H264/big_buck_bunny_1080p_H264_AAC_25fps_7200K_short.MP4
 $HOME/bin/ffprobe $file
 sudo apt-get install pv -y > /dev/null 2>&1
 
@@ -8,10 +9,10 @@ $HOME/bin/ffmpeg -i $file -y \
   -f matroska \
   -map 0:0 -map 0:1  \
   -c:v libx265 \
-  -preset fast \
+  -preset ultrafast \
   -scodec copy -threads 3 -strict experimental \
   -c:a copy \
-  -x265-params crf=22:crf-min=15:crf-max=22:pools=4 \
+  -x265-params crf=18:crf-min=15:crf-max=18:pools=4 \
   - | pv > $file.mkv
   
 echo '************* convert done ****************'
