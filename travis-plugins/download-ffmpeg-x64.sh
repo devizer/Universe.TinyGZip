bc3cb69b0372024451c1dbeff402784cda6f1699
devizer <vgoyda@gmail.com>
2016-08-03 08:22:21 +0300

    update

---
+++ travis-plugins/download-ffmpeg-x64.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+set -e
+old=$(pwd)
+dir=$(tempfile -pffd-)
+file=$(tempfile -pff-)
+mkdir -p $dir-files
+
+echo download as $file.tar.xz
+wget -O $file.tar.xz http://johnvansickle.com/ffmpeg/builds/ffmpeg-git-64bit-static.tar.xz
+
+echo extract to: $dir-files
+tar xJf $file.tar.xz -C $dir-files
+rm $file.tar.xz
+
+echo See $dir-files
+ls -L $dir-files
+
+mkdir -p $HOME/bin
+cd $dir-files
+cd ffmpeg*
+cp * $HOME/bin
+cd $pwd
+rm -rf $dir-files
+rm -f $file
+rm -f $dir
