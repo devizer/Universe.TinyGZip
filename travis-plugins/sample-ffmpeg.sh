d7def2e37060b9fb544046ab558d2427900fbb51
devizer <vgoyda@gmail.com>
2016-08-03 09:02:36 +0300

    update

---
+++ travis-plugins/sample-ffmpeg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+file=/tmp/big_buck_bunny_1080p_H264_AAC_25fps_7200K.mp4
+wget -O http://download.openbricks.org/sample/H264/big_buck_bunny_1080p_H264_AAC_25fps_7200K.MP4
+$HOME/bin/ffprobe $file
+
