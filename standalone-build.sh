15a59b5ce686d6ffc0303e9d76bbbd18c5f4198f
Volodymyr Hoida <devizer@users.github.com>
2017-12-03 18:34:49 +0300

    update

--- standalone-build.sh
+++ standalone-build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-xbuild $TRAVIS_SOLUTION /t:Rebuild /p:Configuration=Release /verbosity:minimal
-xbuild $TRAVIS_SOLUTION /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
+nuget restore
+xbuild /t:Rebuild /p:Configuration=Release /verbosity:minimal
+xbuild /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
 mono --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=Universe.TinyGZip.Tests/bin/Release Universe.TinyGZip.Tests/bin/Release/Universe.TinyGZip.Tests.dll
