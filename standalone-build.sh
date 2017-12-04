ddd341520a8d263059a7c239f6ef337541e29df1
Volodymyr Hoida <devizer@users.github.com>
2017-12-04 08:19:29 +0300

    update

--- standalone-build.sh
+++ standalone-build.sh
@@ -2,4 +2,5 @@
 nuget restore
 xbuild /t:Rebuild /p:Configuration=Release /verbosity:minimal
 xbuild /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
+
 mono --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=Universe.TinyGZip.Tests/bin/Release Universe.TinyGZip.Tests/bin/Release/Universe.TinyGZip.Tests.dll
