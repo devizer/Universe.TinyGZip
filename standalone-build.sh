99f4d350928e7d1b790a4aeb9cf1d71223cf9597
Volodymyr Hoida <devizer@users.github.com>
2017-12-07 03:17:58 +0300

    update

--- standalone-build.sh
+++ standalone-build.sh
@@ -3,4 +3,5 @@ nuget restore
 xbuild /t:Rebuild /p:Configuration=Release /verbosity:minimal
 xbuild /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
 
-mono --desktop packages/NUnit.ConsoleRunner.3.4.1/tools/nunit3-console.exe --labels=On --workers=1 --work=Universe.TinyGZip.Tests/bin/Release Universe.TinyGZip.Tests/bin/Release/Universe.TinyGZip.Tests.dll
+pushd packages/NUnit.ConsoleRunner.*/tools; export RUNNER_PATH=$(pwd); popd; echo RUNNER_PATH: $RUNNER_PATH;
+mono --desktop $RUNNER_PATH/nunit3-console.exe --labels=On --workers=1 --work=Universe.TinyGZip.Tests/bin/Release Universe.TinyGZip.Tests/bin/Release/Universe.TinyGZip.Tests.dll
